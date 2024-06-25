930367de09aa25f89a4898e64556d6093719a95e
BurakKontas <aburakkontas@trakya.edu.tr>
2024-06-25 15:35:58 +0300

    chore: Add useAuth hook to useStartup.tsx and handle token refresh

--- src/hooks/useStartup/useStartup.tsx
+++ src/hooks/useStartup/useStartup.tsx
@@ -4,6 +4,7 @@ import { AuthService } from "../../service/Auth";
 import { useAppDispatch } from "@Redux/hooks";
 import auth from "@Redux/Auth";
 import credit from "@Redux/Credit";
+import { useAuth } from "@Hooks/useAuth";
 
 export const StartupContext = createContext<StartupProviderValueType>({
 })
@@ -13,11 +14,19 @@ const StartupProvider: React.FC<StartupProviderPropsType> = (props) => {
     async function startup() {
         var authService = new AuthService();
         var isValid = await authService.validateToken();
-        if (isValid.success) {
+        if (isValid.value.isValid) {
             dispatch(auth.actions.login());
             dispatch(credit.async_thunks.fetchCredit())
         }
-        else dispatch(auth.actions.logout());
+        else {
+            var refreshToken = localStorage.getItem('refreshToken');
+            if(refreshToken !== null) {
+                await authService.refreshToken();
+                dispatch(auth.actions.login());
+            } else {
+                dispatch(auth.actions.logout());
+            }
+        }
     }
     
     React.useEffect(() => {
